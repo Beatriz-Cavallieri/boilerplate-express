940ce5feaddd9f52c5cf0ec1ff7046879b964361
Beatriz Cavallieri <bfcavallieri@gmail.com>
2024-09-23 12:14:25 -0300

    Mount the express.static() middleware to the path /public with app.use().

--- myApp.js
+++ myApp.js
@@ -3,9 +3,16 @@ let app = express();
 
 console.log("Hello World");
 
+const PUBLIC_PATH = __dirname + '/public';
+const INDEX_PATH = __dirname + '/views/index.html';
+
+app.use("/public", express.static(PUBLIC_PATH));
+
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(INDEX_PATH);
 })
+
+
 
 
 
