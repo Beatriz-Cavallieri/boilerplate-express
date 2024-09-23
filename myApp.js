6cea67bcaa3f33a53b3f9989cbcf6c8a5f45a962
Beatriz Cavallieri <bfcavallieri@gmail.com>
2024-09-23 13:23:45 -0300

    removes uppercase function

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@ app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE === "uppercase") {
         message = "Hello json".toUpperCase();
     }
-    else message = "Hello json".toUpperCase();
+    else message = "Hello json"
     res.json({ message })
 });
 
