2236d70ed85e17e6ece5de24276cb64f01d052ab
Beatriz Cavallieri <bfcavallieri@gmail.com>
2024-09-23 12:37:39 -0300

    Serve the object {"message": "Hello json"} as a response, in JSON format, to GET requests to the /json route.

--- myApp.js
+++ myApp.js
@@ -8,6 +8,10 @@ const INDEX_PATH = __dirname + '/views/index.html';
 
 app.use("/public", express.static(PUBLIC_PATH));
 
+app.get("/json", (req, res) =>{
+    res.json({"message": "Hello json"});
+})
+
 app.get("/", (req, res) =>{
     res.sendFile(INDEX_PATH);
 })
